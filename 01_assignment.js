ea3f1d6a770b450f1d588047fe0ca9027d0dd52d
Sachink2801 <sachinkadam2801@gmail.com>
2022-10-29 02:15:36 +0530

    using Console object method assignment
    using Console object method assignment

---
+++ 01_assignment.js
@@ -0,0 +1,6 @@
+console.log("Hello");
+console.info("My name is Sachin Kadam");
+console.info("i am Mechanical engineer");
+console.log("strength-I always focused towards work or anything what iam doing ")
+console.log("30=30=60");
+console.log("40*40=1600");
