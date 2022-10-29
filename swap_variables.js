c5e4135af07558dee3ab04f76b8c1e940643d733
Sachink2801 <sachinkadam2801@gmail.com>
2022-10-29 14:24:33 +0530

    swap two number using temp
    swap two number using temp

---
+++ swap_variables.js
@@ -0,0 +1,14 @@
+var number1=100;
+var number2=200;
+console.log("Before swap ==============");
+console.log(number1 , number2);
+
+var temp=number1;
+number1=number2;
+number2=temp;
+console.log("After swap==================");
+console.log(number1, number2);
+
+var green_vegetable= true;
+var fast_food=false;
+console.log(green_vegetable);
