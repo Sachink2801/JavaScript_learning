16c4ee9f76510e1865348d7f30bff2d77b2a2517
Sachink2801 <sachinkadam2801@gmail.com>
2022-10-29 16:33:00 +0530

    datatypes using typeof operetor .
    datatypes using typeof operetor .
    diff between null and undefine

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+var s_name="sachin";
+var age=23;
+var city="Mumbai";
+var Married_status=false;
+
+console.log(typeof s_name);
+console.log(typeof age);
+console.log(typeof city);
+console.log(typeof Married_status);
+console.log("===========================");
+var my_phoneNumber=8459234280;
+console.log(typeof my_phoneNumber);
+console.log("============================");
+var p_name;
+console.log(typeof p_name);
+console.log("============================");
+var p_age;  // when we not declare any value then we get undefine 
+console.log(typeof p_age);
+console.log("============================");  // the diff between null & undefine is = type of null is object & type of undefine is undefine 
+                                              //null mince simply nothing & undefine mince when variable is declare and not assign with value is call undefine         
+
+var city=null; //when we have null the type of null is object 
+console.log(typeof city);
+
+
