617edeee0aae0a5eb5f17b3747c66e46df21be3a
Sachink2801 <sachinkadam2801@gmail.com>
2022-10-29 21:59:48 +0530

    Function
    using function

---
+++ 03_function.js
@@ -0,0 +1,21 @@
+function sachin(money){
+    console.log("Please go to market with Rs.",money);
+    console.log("bring some vegetable ");
+
+return "vegetable bag"
+}
+   var veg=sachin(500);
+   console.log(veg);
+
+   console.log("=========================================================")
+
+   function my_self(age){
+    console.log("hello guys my age ",age);
+    console.log("i am sachin ");
+    console.log("i am Mechanical");
+    console.log("i am learning javaScript in CODEMIND ");
+    
+    return "name"
+   }
+
+ var details =my_self(23);
