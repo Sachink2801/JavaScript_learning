ac6fb3599ef8fea0f24a8221bdd4cf0ca34e3add
Sachink2801 <sachinkadam2801@gmail.com>
2022-10-30 02:45:55 +0530

    type of oprater
    type of oprater

---
+++ typeof_oprater.js
@@ -0,0 +1,12 @@
+var name="sachin";
+var age=23;
+var Married_status=false;
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof Married_status)
+
+console.log("diff between null and undefine");
+var val1=null;
+var val2;
+console.log(val1);
+console.log(val2);
