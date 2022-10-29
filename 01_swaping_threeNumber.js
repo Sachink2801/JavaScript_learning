c612c252cabf57fddb7d33a9dded6d3f857db50b
Sachink2801 <sachinkadam2801@gmail.com>
2022-10-29 21:58:41 +0530

    swaping three number using temp
    swaping three number using temp assignment 01

---
+++ 01_swaping_threeNumber.js
@@ -0,0 +1,13 @@
+var sachin="sachin";
+var sanjay="sanjay";
+var kadam="kadam";
+console.log("Before swap========================");
+console.log(sachin,sanjay,kadam);
+
+var temp=sachin;
+sachin=sanjay;
+sanjay=kadam;
+kadam=temp;
+console.log("After swap==========================");
+console.log(sachin,sanjay,kadam);
+
