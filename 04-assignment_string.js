69632298483dddb51610dbca8666ea9572c71682
Sachink2801 <sachinkadam2801@gmail.com>
2022-10-31 03:44:19 +0530

    04_assignment_string
    04_assignment_string

---
+++ 04-assignment_string.js
@@ -0,0 +1,13 @@
+var str =function(){
+ var dream_company_name="AMDOCS";
+ console.log(`My dream company name is :${dream_company_name}`);
+ var hobby1="singing";
+ var hobby2="Photography";
+ var hobby3="Learn new thing";
+ console.log(`My 1st hobby is:${hobby1}`);
+ console.log(`MY 2nd hobby is:${hobby2}`);
+ console.log(`My 2rd hobby is:${hobby3}`);
+ console.log(`All 3 hobbies concatenated: ${" ",hobby1.concat(" ",hobby2," ",hobby3)}`);
+
+}
+str();
