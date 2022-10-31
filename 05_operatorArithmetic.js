1efa353fecf2f9ab951de69c2e388b0137c7700d
Sachink2801 <sachinkadam2801@gmail.com>
2022-10-31 19:20:27 +0530

    Arithmetic operator

---
+++ 05_operatorArithmetic.js
@@ -0,0 +1,40 @@
+var value1=100;
+var value2=200;
+var result=value1+value2;
+console.log(`Addition of this number is: ${result}`);
+
+var value1=45;
+var value2=50;
+var result=value1*value2;
+console.log(`Multiplication of this number is : ${result}`);
+
+
+var value1=200;
+var value =20;
+var result1=value1/value2;
+var result2=value1%value2;
+console.log(`Division of this number is :${result1}`);
+console.log(`Remainder of this number is :${result2}`);
+
+console.log("======= Module% operator===========");
+var val1=10;
+var val2=5;
+console.log(val1%2);
+console.log(val2%2);
+
+console.log("====== ++ Increment===============");
+num1=10;
+num2=20;
+
+var incr=num1+1;
+console.log(incr);
+console.log("===========================");
+var incr= ++num1;
+console.log(incr);
+console.log(`increment number: ${incr}`);
+
+console.log("======= --Decrement================");
+num2=10;
+var decr = --num2;
+console.log(decr);
+console.log(`decrement number :${decr}`);
