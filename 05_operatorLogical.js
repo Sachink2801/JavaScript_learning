880f433d5d1b7e4474e0dbc2cbfeca147e5e952f
Sachink2801 <sachinkadam2801@gmail.com>
2022-11-01 02:31:47 +0530

    05_using ternary operator
    comparison operator

---
+++ 05_operatorLogical.js
@@ -0,0 +1,11 @@
+console.log("+++++++++++++++++ternary operator ( ? : ) +++++++++++++");
+
+var num1=20;
+var num2=10;
+var result=num1>num2 ? "positive" : "negative";
+console.log(result);
+
+var arg1="sachin";
+var arg2="kadam";
+var result= arg1==arg2 ? "positive" : "negative" ;
+console.log(result);
