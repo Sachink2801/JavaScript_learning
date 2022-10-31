e8283f32ebb8e9f8e516d242616b2f5b3ce90703
Sachink2801 <sachinkadam2801@gmail.com>
2022-11-01 02:07:26 +0530

    comparison operator
    using all comparison operator and difference between == &=== operator.

---
+++ 05_operatorComparison.js
@@ -0,0 +1,20 @@
+num1=50;
+num2=40;
+
+console.log("num1<num2=====>" ,num1<num2);
+console.log(`num1>num2====>${num1>num2}`);
+console.log(`num1==num2====> ${num1==num2}`);
+console.log(`num1!=num2=====> ${num1!=num2}`);
+
+num3=40;
+
+console.log(`num2<=num3====>${num2<=num3}`);
+console.log(`num2<num3====> ${num2<num3}`);
+console.log(`num1>=num2=====>${num1>=num2}`);
+
+console.log("*******************== operator & === operator*****************");
+num1=20;
+num2="20";
+console.log(`num1==num2 ===>${num1==num2}`);
+
+console.log(`num1===num2 ===>${num1===num2}`);
