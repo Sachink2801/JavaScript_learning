06d99ba63ff9ebe562b9c8abbdd854741a1c8c1e
Sachink2801 <sachinkadam2801@gmail.com>
2022-11-01 03:27:36 +0530

    05_assignment01
    using ternary operator

--- 05_assignment01.js
+++ 05_assignment01.js
@@ -1,32 +1,31 @@
-var wordLengthSquare = function(arg1){
-   var wordLengthSquare= arg1.length;
-   var square= wordLengthSquare * wordLengthSquare ;
-   console.log(`Length of ${arg1} is: ${wordLengthSquare} and square is : ${square}`);
-}
-wordLengthSquare("JavaScript");
-wordLengthSquare("Google");
-wordLengthSquare("Developer");
-
-console.log("==============================================================================");
-
-function stringOperation(){
-    var string1="I am angular developer";
-    console.log(`Given string is ${string1}`);
-
-    var stringLen=string1.length;
-    console.log(`Total string Length is :${stringLen}`);
-
-    var spilt1=string1.split(" ");
-    console.log(spilt1);
-
-    var totalWords=spilt1.length;
-    console.log(`Total no of word in string is : ${totalWords}`);
-
-    var total=stringLen/totalWords;
-    console.log(`Result after dividing length of string with total number of word in string is :${total} `);
-
-    return string1.split(" ").reverse().join(" ");
-
+var compareOperator = function(arg1,arg2){
+   var result= arg1>arg2 ? arg1 : arg2;
+   console.log(`Greatest number among two number ${arg1},${arg2} that is : ${result}`);
 
 }
- var string2 =console.log(`Reverse string is: ${stringOperation()}`);
+compareOperator(10,-10);
+compareOperator(800,899);
+
+console.log("=================================================================================");
+
+var evenodd=function(arg1){
+
+    var result=(arg1%2==0) ? true : false;
+    console.log(`Given number ${arg1}:${result}`);
+}
+
+evenodd(29);
+evenodd(44);
+evenodd(0);
+evenodd(101);
+
+console.log("==================================================================================");
+
+var evenOddStr= function(arg1){
+    var len=arg1.length;
+    var str=(len%2==0) ? "Even" : "Odd"
+    console.log(`Word ${arg1} length is : ${str}`);
+}
+evenOddStr("JavaScript");
+evenOddStr("developer");
+evenOddStr("Google");
