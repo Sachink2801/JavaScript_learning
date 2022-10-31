9cf150c96b2f8d7c7f67aa1cc1d6fc114311782f
Sachink2801 <sachinkadam2801@gmail.com>
2022-10-31 19:41:40 +0530

    Assignment operator
    Assignment operator

---
+++ 05_operatorAssignment.js
@@ -0,0 +1,32 @@
+console.log("========== compound addition +=  ================");
+num1=10;
+num2=20;
+num1+=num2;  //num1=num1+num2
+console.log(`compound addition += is :${num1}`);
+
+console.log("=========== compound subtraction -= =============");
+num1=50;
+num2=40;
+num1-=num2;
+console.log(`compound subtraction -= :${num1}`);
+
+console.log("=========== compound Multiplication *= ===========");
+num1=50;
+num2=50;
+num1*=num2;
+console.log(`compound Multiplication *=: ${num1}`);
+
+console.log("=========== compound Division /= ===========");
+num1=50;
+num2=50;
+num1/=num2;
+console.log(`compound Division /=: ${num1}`);
+
+console.log("=========== compound Modulus %= ===========");
+num1=50;
+num2=100;
+num1%=num2;
+console.log(`compound Multiplication %=: ${num1}`);
+
+
+
