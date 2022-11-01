88cce63b6886612a808f85e3043c2c55271c1a46
Sachink2801 <sachinkadam2801@gmail.com>
2022-11-02 02:50:25 +0530

    using NaN =NOT a NUMBER

---
+++ 05_NaN.js
@@ -0,0 +1,29 @@
+var myResult=0/0;
+console.log(myResult);
+
+var myName="Sachin";
+var myAge="23";
+
+console.log(typeof myName);
+console.log(typeof myAge);
+
+var myAgeConvert = +myAge;
+console.log( myAgeConvert ,typeof myAgeConvert);
+
+var myNameConvert = +myName;
+console.log(myNameConvert ,typeof myNameConvert );
+console.log("=============================Using (+) in different ways  ===================================================");
+//Addition
+var val1=200;
+var val2=300;
+console.log(val1+val2);
+//Concat
+var arg1="sachin";
+var arg2="kadam";
+console.log(arg1+arg2);
+//conversion
+var val1=+"20";    
+var val2=+"30";
+console.log(val1+val2);
+
+
