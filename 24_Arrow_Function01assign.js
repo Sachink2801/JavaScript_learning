678d3da1b2f03d061061feb45cf3c0a44399ee00
Sachink2801 <sachinkadam2801@gmail.com>
2022-11-04 11:01:32 +0530

    using Arrow Function
    perform addition , Multiplication using Arrow Function

---
+++ 24_Arrow_Function01assign.js
@@ -0,0 +1,32 @@
+var s=() =>{
+    console.log("4 th November assignment of: sachin kadam");
+}
+s();
+
+console.log("=======Arrow function with no argument and return value ======");
+ var display=() =>{
+    console.log("Good Morning,TOday is Friday");
+ }
+ display();
+
+ console.log("=====Arrow Function with three argument and no return value=====");
+
+ var multiplication = (a,b,c=5) =>{
+    var d=a*b*c;
+    console.log(`Multiplication of ${a},${b} and ${c} is ${d}`);
+ }
+ multiplication(5,2,3,);
+ multiplication(10,4);
+
+ console.log("====Arrow Function with Five argument and return function===");
+
+ var addition =(a,b,c,d,e) => {
+       return a+b+c+d+e;
+
+ }
+ var result=addition(100,100,200,349,756);
+ var res=addition("I am "," learning "," ES6 "," Feature "," in depth ");
+ console.log(res);
+ console.log(`Addition of number 100,100,200,349 and 756 is : ${result} and ${res}`);
+
+
