069e906c2433fc09f40f81b321a91a5dd312abc5
Sachink2801 <sachinkadam2801@gmail.com>
2022-11-04 19:44:14 +0530

    Array basic
    using different method like length, check index position ,& Replace index value .

--- 07_arrayBasic.js
+++ 07_arrayBasic.js
@@ -1,11 +1,14 @@
 var arrayNumber=["sachin",28,01,2000,2.0];
 console.log(arrayNumber);
 console.log("=====Check Total Length of Array=====");
+
 var arrayLength=arrayNumber.length;
 console.log(`Length of Array is ====> ${arrayLength}`);
+
 console.log("====Check index position value==== ");
 var result = arrayNumber[3];
 console.log(`Check index position at 3 is ====> ${result}`);
+
 console.log("====Replace index value ====");
  var replaceVal=arrayNumber[0]="Prince";
 console.log(`Update value at position 0 is =====> ${replaceVal}`);
