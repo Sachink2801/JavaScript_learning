2fedd8c96e255b3d4bf9298d766445d439e80679
Sachink2801 <sachinkadam2801@gmail.com>
2022-11-05 15:40:44 +0530

    While loop
    syntax and table of 5 using while loop

---
+++ 06_while_loop.js
@@ -0,0 +1,20 @@
+console.warn("==== while loop ====");
+var count=10;
+while(count>=0){
+    console.log(count);
+    count--;
+}
+
+// console.warn("===while loop syntax===");
+// //initialization
+// while(condition){
+
+//     //update expression
+// }
+
+console.warn("===Table of 5===");
+var count =5;
+while(count <= 50){
+    console.log(count);
+    count=count + 5;
+}
