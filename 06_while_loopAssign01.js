502f06e1b1322d974bab07c30474203f146f0899
Sachink2801 <sachinkadam2801@gmail.com>
2022-11-05 20:31:28 +0530

    while loop
    assignment 01

---
+++ 06_while_loopAssign01.js
@@ -0,0 +1,29 @@
+console.warn('=== Print no from 5 to 15 increment by 1 ===');
+var count =5;
+while (count<=15) {
+    console.log(count);
+    count++;
+}
+
+console.warn('=== Print no from 50 to 40 decrement by 1 ===');
+var count =50;
+while(count>=40){
+    console.log(count);
+    count--;
+}
+
+console.warn("=== print table 5 ====");
+var count=5;
+while(count<=50){
+    console.log(count);
+    count=count+5;
+}
+
+console.warn("=== print table 10 ====");
+var count=10;
+while(count<=100){
+    console.log(count);
+    count=count+10;
+}
+
+
